8b8de7cb803d0e9a5fd949bc616da731e883cc02
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2022-01-02 20:56:36 +0600

    initial commit

---
+++ gadgethub/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gadgethub project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gadgethub.settings')
+
+application = get_wsgi_application()
