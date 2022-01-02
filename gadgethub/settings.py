e8382b48b63bfbd303ed8a9d224d4d2b6da5a85f
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2022-01-03 00:06:02 +0600

    fdsF

--- gadgethub/settings.py
+++ gadgethub/settings.py
@@ -126,3 +126,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'gadgethub/static')
 ]
+
+#Media Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+
+
