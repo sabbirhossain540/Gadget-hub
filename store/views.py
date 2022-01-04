674ba00b41ceae6881a0c77abf8b34eef989ac1d
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2022-01-04 23:42:07 +0600

    end add to cart

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from carts.models import CartItem
+from carts.views import _cart_id
 
 from category.models import Category
 from .models import Product
@@ -32,11 +34,14 @@ def store(request, category_slug = None):
 def products_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+
         #return HttpResponse(single_product)
     except Exception as e:
         raise e
 
     context = {
-        'single_product' : single_product
+        'single_product' : single_product,
+        'in_cart' : in_cart,
     }
     return render(request, 'store/product_detail.html', context)
