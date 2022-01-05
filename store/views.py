e8276e30d36faea804be72c6a6c5c81e65e0a899
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2022-01-05 21:30:35 +0600

    dsds

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.core import paginator
 from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 from carts.models import CartItem
 from carts.views import _cart_id
 
@@ -23,7 +24,7 @@ def store(request, category_slug = None):
 
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         paginator = Paginator(products, 6)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
@@ -55,3 +56,21 @@ def products_detail(request, category_slug, product_slug):
         'in_cart' : in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains = keyword) | Q(product_name__icontains = keyword))
+            product_count = products.count()
+
+        #return HttpResponse(products)
+
+    context = {
+       'page_obj' : products,
+       'product_count' : product_count
+    }
+
+    return render(request, 'store/store.html', context)
