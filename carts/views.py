e64a4c1e44bae2791a05b6be4265eb909909968b
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2022-01-06 00:01:21 +0600

    sd

--- carts/views.py
+++ carts/views.py
@@ -17,9 +17,12 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
-    color = request.GET['color']
-    size = request.GET['size']
-    return HttpResponse(color)
+    if request.method == 'POST':
+        color = request.POST['color']
+        size = request.POST['size']
+        return HttpResponse(color)
+
+    
 
     product = Product.objects.get(id=product_id)
     
