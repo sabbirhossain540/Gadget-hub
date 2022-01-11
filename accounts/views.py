36e0ffe913f1689fe25ea37350f07e9d6c631be7
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2022-01-11 21:41:18 +0600

    user activation

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,14 @@ from .forms import RegistrationForm
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 
+#EMAIL VARIFICATION
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
 
 # Create your views here.
 
@@ -24,6 +32,22 @@ def register(request):
             user = Account.objects.create_user(first_name= first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
+
+            #User Activation
+            current_site = get_current_site(request)
+            mail_subject = "Please activate Your account"
+            message = render_to_string('accounts/account_varification_email.html', {
+                'user':user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+
             messages.success(request, 'Registration Successfull')
             return redirect('register')
     else:
--- gadgethub/settings.py
+++ gadgethub/settings.py
@@ -141,3 +141,11 @@ MESSAGE_TAGS = {
 }
 
 
+#SMTP Configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bcadc555@gmail.com'
+EMAIL_HOST_PASSWORD = 965874321
+EMAIL_USE_TLS = True
+
+
