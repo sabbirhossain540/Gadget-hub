c9e77234260d0d6b22833b31d81611defbd897c4
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2022-01-13 22:33:37 +0600

    dsd

---
+++ orders/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import redirect, render
+from carts.models import CartItem
+from .models import Order
+from .forms import OrderForm
+import datetime
+
+# Create your views here.
+def place_order(request , total = 0, quantity = 0):
+    current_uesr = request.user
+    
+    cart_items = CartItem.objects.filter(user=current_uesr)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
+    
+    grand_total = 0
+    tax = 0
+
+
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    
+    tax = (2 * total)/100
+    grand_total = total + tax
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            data = Order()
+            data.user = current_uesr
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.phone = form.cleaned_data['phone']
+            data.email = form.cleaned_data['email']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.country = form.cleaned_data['country']
+            data.state = form.cleaned_data['state']
+            data.city = form.cleaned_data['city']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+
+            #generate order no
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr,mt,dt)
+            current_date = d.strftime("%Y%m%d")
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+            data.save()
+            return redirect('checkout')
+        else:
+            return redirect('checkout')
+
+
