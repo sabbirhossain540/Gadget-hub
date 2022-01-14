e626e4e966c97bcf6baa71b49b2def894ad3855f
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2022-01-14 23:39:01 +0600

    dsd

--- store/views.py
+++ store/views.py
@@ -6,8 +6,10 @@ from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Q
 from carts.models import CartItem
+from orders.models import OrderProduct
 from carts.views import _cart_id
 from .forms import ReviewForms
+from .models import ReviewRating
 
 
 
@@ -56,9 +58,19 @@ def products_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
 
+    try:
+        orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+    except OrderProduct.DoesNotExist:
+        orderproduct = None
+
+    #Get The Review
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+
     context = {
         'single_product' : single_product,
         'in_cart' : in_cart,
+        'orderproduct' : orderproduct,
+        'reviews':reviews,
     }
     return render(request, 'store/product_detail.html', context)
 
