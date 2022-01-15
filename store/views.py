d6c51bd581ea5ab654dafabedadf25b4b22873e9
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2022-01-15 22:06:21 +0600

    dsd

--- store/views.py
+++ store/views.py
@@ -9,7 +9,7 @@ from carts.models import CartItem
 from orders.models import OrderProduct
 from carts.views import _cart_id
 from .forms import ReviewForms
-from .models import ReviewRating
+from .models import ProductGallery, ReviewRating
 
 
 
@@ -71,11 +71,15 @@ def products_detail(request, category_slug, product_slug):
     #Get The Review
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
+    #get product gellary
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
         'single_product' : single_product,
         'in_cart' : in_cart,
         'orderproduct' : orderproduct,
         'reviews':reviews,
+        'product_gallery': product_gallery,
     }
     return render(request, 'store/product_detail.html', context)
 
