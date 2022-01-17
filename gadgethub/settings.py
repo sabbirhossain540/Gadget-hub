ee42ec9700557abfb2763e5a1c1101bdcb10f758
Sabbir Hossain <36231104+sabbirhossain540@users.noreply.github.com>
2022-01-17 22:34:10 +0600

    Update settings.py

--- gadgethub/settings.py
+++ gadgethub/settings.py
@@ -14,6 +14,9 @@ import os
 from pathlib import Path
 from decouple import config #For secure site configure
 
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
