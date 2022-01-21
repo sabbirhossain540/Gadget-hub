a283b398ecb89c83b48bf128092374bee6b78e04
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2022-01-21 23:07:33 +0600

    save

--- store/models.py
+++ store/models.py
@@ -80,6 +80,7 @@ class ReviewRating(models.Model):
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.subject
